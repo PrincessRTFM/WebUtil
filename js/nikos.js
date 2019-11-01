a66190a47e09aafe8091b2f53ad8108488aed99b
Lilith Song <lsong@princessrtfm.com>
2019-11-01 10:56:03 -0400

    Added code comments

--- js/nikos.js
+++ js/nikos.js
@@ -59,7 +59,10 @@ $(() => {
 	const startClearHelpTimer = function() {
 		unhelpTimer = setTimeout(setHelpText, 500);
 	};
-	const help = (selector, text) => $(selector).hover(setHelpText.bind(setHelpText, text), startClearHelpTimer);
+	const help = (selector, text) => $(selector).hover(
+		setHelpText.bind(setHelpText, text),
+		startClearHelpTimer
+	);
 	$('.soften, textarea').addClass('ui-corner-all');
 	const error = $('#errortext');
 	const canvas = document.getElementById('photoshop');
@@ -68,15 +71,17 @@ $(() => {
 	const background = new Image();
 	const message = document.getElementById('message');
 	const refreshRender = () => {
-		const MAX_LINE_LENGTH = 465;
+		const MAX_LINE_LENGTH = 465; // Magic. Do not change.
 		draw.clearRect(0, 0, canvas.width, canvas.height);
 		if (background.complete) {
 			draw.drawImage(background, 0, 0);
 		}
 		else {
+			// If we haven't got the background image loaded in yet, cancel the render entirely
 			return;
 		}
 		// Render the chosen FACE at X=496 Y=16
+		// Technically, any 96x96 image will do... so this could be tweakable for future ideas pretty easily
 		if (document.getElementById('selected')) {
 			try {
 				draw.drawImage(document.getElementById('selected'), 496, 16);
@@ -87,10 +92,14 @@ $(() => {
 			}
 		}
 		// Render the text from X=20 Y=17 to X=485 (465px)
+		// This 465px is where the magic MAX_LINE_LENGTH constant came from, and why it's that exact number
+		// The top/left start point and font size were carefully determined through testing in GIMP to be the actual values used in the game itself
+		// The multi-line splitting was done manually for a closest-approximation look, but if someone (hi there!) can send me the correct values,
+		// it's easy enough to update them in here.
 		if (message.value) {
-			const lines = message.value.split("\n");
-			for (let lineNo = 0; lineNo < lines.length; lineNo++) {
-				if (lineNo >= 3) {
+			const lines = message.value.split("\n"); // Split on actual line breaks to keep any user-defined lines intact
+			for (let lineNo = 0; lineNo < lines.length; lineNo++) { // Iterate through the lines, NOT using any kind of foreach loop - see below for why
+				if (lineNo >= 3) { // We can only fit three lines into a single image, more will get cut off
 					error.show();
 					break;
 				}
@@ -98,6 +107,11 @@ $(() => {
 					error.hide();
 				}
 				let line = lines[lineNo];
+				// Here's a bit of magic - it adds line breaks where needed, but still maintains the user's original line breaks too!
+				// If the rendered line is/would be too wide, we split it into words (splitting on whitespace, although I could do more)
+				// and slowly snip words off the end, one by one, until the line is within the max width. Then, all of the words that had
+				// to be removed are spliced into the lines array immediately following the current line, which is why we had to use the
+				// basic for loop above instead of any kind of for/in or foreach method instead - the array is being modified inside the loop.
 				if (draw.measureText(line).width > MAX_LINE_LENGTH) {
 					const words = line.split(/\s/u);
 					for (let word = words.length; word > 0; word--) {
@@ -109,13 +123,17 @@ $(() => {
 						}
 					}
 				}
+				// At this point, the line is either within the max width, or there's no whitespace to split on.
+				// The height is a base of 17 pixels down, plus another 28 pixels for every line ABOVE this one.
+				// If the line is still too wide (but has no whitespace to split on) then the text is just smushed.
 				draw.fillText(line, 20, 17 + lineNo * 28, MAX_LINE_LENGTH);
 			}
 		}
 		else {
 			error.hide();
 		}
-		// See if we can render it to a proper image element, so people can select it on mobile and all browsers (IE is not a browser, do not complain about it being broken)
+		// See if we can render it to a proper image element, so people can select it on mobile and all browsers
+		// (IE is not a browser, do not complain about it being broken)
 		try {
 			image.src = canvas.toDataURL();
 			image.style.display = 'block';
@@ -125,18 +143,22 @@ $(() => {
 			image.style.display = 'none';
 			canvas.style.display = 'block';
 		}
+		// That's in a try/catch because if you're running this locally, not off a web server, then for security reasons the
+		// canvas contents can't be retrieved as a data URL. In order to sneak around that little issue, the image element
+		// is only shown (and the canvas hidden automatically) if we CAN get the data URL. Otherwise, if we can't, the canvas
+		// element is shown and the image is automatically hidden.
 	};
 	const faceList = $('#faces');
-	FACES.forEach(filename => {
+	FACES.forEach(filename => { // This makes it really easy to add new faces - upload a 96x96 image in the right place and extend the array
 		const face = $(`<img class="face" src="img/expressions/${filename}.png" />`);
 		faceList.append(face);
 	});
-	draw.font = '20pt TerminusTTF';
+	draw.font = '20pt TerminusTTF'; // Loaded off the css/ directory, in case you don't have it natively
 	draw.textBaseline = 'top';
 	draw.fillStyle = '#ffffff';
-	background.addEventListener('load', refreshRender, false);
+	background.addEventListener('load', refreshRender, false); // As soon as the background image loads, try to render
 	background.src = "img/niko-background.png";
-	faceList.children('img').on('click', function setExpression() {
+	faceList.children('img').on('click', function setSelectedFace() {
 		$('.face#selected').attr('id', '');
 		this.id = 'selected';
 		refreshRender();
