3d7fcfdf6e8f9c9c3e298d76075bd68538fea0d4
Lilith Song <lsong@princessrtfm.com>
2019-12-21 06:23:48 -0500

    Fixed the header block in TCPRA script

--- js/monkey/twitch-channel-point-reward-autograbber.user.js
+++ js/monkey/twitch-channel-point-reward-autograbber.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Twitch Channel Point Reward Autograbber
-// @namespace    Lilith
+// @namespace    PrincessRTFM
 // @version      1.0.0
 // @description  Automatically clicks the channel points reward chest whenever it shows up (checks every two seconds)
-// @author       PrincessRTFM
+// @author       Lilith Song <lilith@princessrtfm.com>
 // @match        https://www.twitch.tv/*
-// @grant        GM_info
 // @updateURL    https://gh.princessrtfm.com/js/monkey/twitch-channel-point-reward-autograbber.user.js
 // ==/UserScript==
 
