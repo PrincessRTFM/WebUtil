ae8ddb350f784939b09b03c2a5baf8becfc6c30c
Lilith Song <lsong@princessrtfm.com>
2020-06-25 18:32:40 -0400

    Updated eslint rules (again)

--- .eslintrc.js
+++ .eslintrc.js
@@ -408,12 +408,6 @@ const config = {
 		"unicorn/prefer-query-selector": "error",
 		"unicorn/no-abusive-eslint-disable": "error",
 		"import/no-unresolved": "error",
-		"import/named": [
-			"error",
-			{
-				commonjs: true,
-			},
-		],
 		"import/no-absolute-path": "error",
 		"import/no-dynamic-require": "warn",
 		"import/no-internal-modules": "warn",
